e11d9d57ee3b0640fb6453c7bf097e661979d288
Nouman76 <25100301@lums.edu.pk>
2025-05-04 17:39:17 +0500

    merging deo_2 to main for rendering

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri = process.env.MONGODB_URI; 
+const uri = 'mongodb+srv://25100159:cyostz0VrFONapW8@cluster0.8pxma.mongodb.net/'; 
 mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("Error connecting to MongoDB:", err));
